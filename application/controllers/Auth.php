608d2c023fdf745f7ab7bfba2a9dbd136690f532
unknown <lintangpurnama1@gmail.com>
2021-08-23 16:39:10 +0700

    dashboard

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -48,7 +48,13 @@ class Auth extends CI_Controller
                         'role_id' => $user['role_id']
                     ];
                     $this->session->set_userdata($data);
-                    redirect('user');
+                    if ($user['role_id'] == 1) {
+                        redirect('admin');
+                    } else {
+                        redirect('user');
+                    }
+
+
                     $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">This Email has not been activated!</div>');
                     redirect('auth');
                 } else {
