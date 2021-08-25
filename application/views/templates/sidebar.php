6d56edcb72092a2714bfab90cfc65732e6cd1ac3
unknown <lintangpurnama1@gmail.com>
2021-08-25 23:34:46 +0700

    query category

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -11,9 +11,42 @@
 
     <!-- Divider -->
     <hr class="sidebar-divider">
-    <div class="sidebar-heading">
-        Administrator
-    </div>
+
+
+    <!-- Query Menu -->
+    <?php
+    // $role_id = $this->session->userdata('role_id');
+    // $queryMenu = "SELECT `user_menu`.`id` , `menu` 
+    //                 FROM `user_menu`JOIN `user_access_menu` 
+    //                 ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+    //             WHERE `user_access_menu`.`role_id`= $role_id
+    //             ORDER BY `user_acces_menu`.`menu_id` ASC
+    //    ";
+    // $menu = $this->db->query($queryMenu)->result_array();
+    // var_dump($menu);
+    // die;
+
+
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT `user_menu`.`id`, `menu`
+                    FROM `user_menu` JOIN `user_access_menu`
+                     ON `user_menu`.`id` = `user_access_menu`.`menu_id`
+                WHERE `user_access_menu`.`role_id` = $role_id
+                ORDER BY `user_access_menu`.`menu_id` ASC 
+                ";
+    $menu = $this->db->query($queryMenu)->result_array();
+
+    ?>
+
+
+
+    <!-- looping menu -->
+    <?php foreach ($menu as $m) :  ?>
+        <div class="sidebar-heading">
+            <?= $m['menu']; ?>
+        </div>
+
+    <?php endforeach; ?>
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
         <a class="nav-link" href="index.html">
