50a7c7795a30c79847d3fbb922a13c6753328b89
unknown <lintangpurnama1@gmail.com>
2021-08-29 00:43:29 +0700

    menambah sub menu

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -38,53 +38,55 @@
 
     ?>
 
-
-
-    <!-- looping menu -->
     <?php foreach ($menu as $m) :  ?>
         <div class="sidebar-heading">
             <?= $m['menu']; ?>
         </div>
 
-    <?php endforeach; ?>
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+        <?php
+        $menuId = $m['id'];
+        $querySubMenu = "SELECT *
+        FROM `user_sub_menu` JOIN `user_menu`
+         ON `user_sub_menu`.`menu_id` = `user_menu`.`id`
+        WHERE `user_sub_menu`.`menu_id` = {$m['id']}
+        AND `user_sub_menu`.`is_active` = 1  
+    ";
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Interface
-    </div>
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-user"></i>
-            <span>My Profile</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+        <?php foreach ($subMenu as $sm) : ?>
+            <?php if ($title == $sm['title']) : ?>
+                <li class="nav-item active">
+                <?php else :  ?>
+                <li class="nav-item">
+                <?php endif; ?>
+                <a class="nav-link" href="<?= base_url($sm['url']); ?>">
+                    <i class="<?= $sm['icon']; ?>"></i>
+                    <span><?= $sm['title']; ?></span></a>
+                </li>
+            <?php endforeach; ?>
+            <hr class="sidebar-divider">
 
 
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('Auth/logout') ?>">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
+        <?php endforeach; ?>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+        <!-- looping menu -->
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+
+
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url('Auth/logout') ?>">
+                <i class="fas fa-fw fa-sign-out-alt"></i>
+                <span>Logout</span></a>
+        </li>
+
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
 
 </ul>
 <!-- End of Sidebar -->
